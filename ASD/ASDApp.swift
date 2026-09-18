38c5bca56c9c5872923a02da1b9f44d08ebad834
Nazar Dydyn <nazardydyn83@gmail.com>
2026-09-18 10:37:43 +0300

    Initial Commit

---
+++ ASD/ASDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASDApp.swift
+//  ASD
+//
+//  Created by Nazar Dydyn on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct ASDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
